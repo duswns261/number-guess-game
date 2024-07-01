9f921d1ce531f62f47268428356a6d707ea38ca2
Cret <duswns261@naver.com>
2024-07-02 00:02:18 +0900

    project completion

--- main.js
+++ main.js
@@ -12,7 +12,18 @@ let computerNum = 0
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
+let resetButton = document.getElementById("reset-button")
+let chanceArea = document.getElementById("chance-area")
+
+let chances = 5
+let gameOver = false
+let history = []
+
 playButton.addEventListener("click", play)
+resetButton.addEventListener("click", reset)
+userInput.addEventListener("focus", function() {
+    userInput.value = "";
+})
 
 function pickRandomNum() {
     computerNum = Math.floor(Math.random() * 100)
@@ -21,6 +32,23 @@ function pickRandomNum() {
 
 function play() {
     let userValue = userInput.value
+    
+    if (userValue < 1 || userValue > 100) {
+        resultArea.textContent = "1과 100 사이 숫자를 입력해 주세요"
+        return
+    }
+
+    if (history.includes(userValue)) {
+        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요."
+        return
+    }
+
+    history.push(userValue)
+
+    chances--
+    chanceArea.textContent = `남은 기회 : ${chances}번`
+    console.log("chance", chances)
+
     if (userValue < computerNum) {
         resultArea.textContent = "UP!"
         console.log("UP!")
@@ -30,7 +58,24 @@ function play() {
     } else {
         resultArea.textContent = "정답!"
         console.log("정답!")
+        gameOver = true
     }
+
+    if (chances < 1) {
+        gameOver = true
+    }
+
+    if (gameOver == true) {
+        playButton.disabled = true
+    }
+}
+
+function reset() {
+    userInput = ""
+
+    pickRandomNum()
+
+    resultArea.textContent = "결과값이 여기 나옵니다."
 }
 
 pickRandomNum();
